61f1ca0f1a4efaec282d46d9dd4edd7fe8b62557
Nguyễn Tuấn Thông <109049076+thongnt55@users.noreply.github.com>
2022-08-01 20:22:15 +0700

    like ajax

--- src/main/java/com/reljicd/controller/LikeController.java
+++ src/main/java/com/reljicd/controller/LikeController.java
@@ -1,5 +1,7 @@
 package com.reljicd.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.reljicd.model.Comment;
 import com.reljicd.model.Post;
 import com.reljicd.model.User;
@@ -14,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
 import java.util.Optional;
@@ -32,30 +35,49 @@ public class LikeController {
     }
 
     @RequestMapping(value = "/Like/{id}", method = RequestMethod.GET)
-    public String Likenew(@PathVariable Long id, Principal principal, Model model) {
-
+    public @ResponseBody String Likenew(@PathVariable Long id, Principal principal, Model model) {
+        ObjectMapper mapper = new ObjectMapper();
+        String ajaxResponse = "";
+        System.out.println(id);
         Optional<Post> post = postService.findForId(id);
-
-        if (post.isPresent()) {
-            Optional<User> user = userService.findByUsername(principal.getName());
+        Optional<User> user = userService.findByUsername(principal.getName());
+        try {
             if (user.isPresent()) {
                 Like like = new Like();
                 like.setUser(user.get());
                 like.setPost(post.get());
-                if( likeService.findLikeByPostAndAndUser(post.get() , user.get()) != null) {
+                if (likeService.findLikeByPostAndAndUser(post.get(), user.get()) != null) {
                     System.out.println("exist");
-                    likeService.deleteLike( post.get().getId(), user.get().getId());
+                    likeService.deleteLike(post.get().getId(), user.get().getId());
                 }
-                else
-                    likeService.save(like);
-               return "redirect:/post/"+ like.getPost().getId();
-
-            } else {
-                return "/error";
+                else likeService.save(like);
+                int countLike = likeService.countLike(Math.toIntExact(post.get().getId()));
+                ajaxResponse = mapper.writeValueAsString(countLike);
             }
-
-        } else {
-            return "/error";
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
+        return ajaxResponse;
+//        if (post.isPresent()) {
+//            Optional<User> user = userService.findByUsername(principal.getName());
+//            if (user.isPresent()) {
+//                Like like = new Like();
+//                like.setUser(user.get());
+//                like.setPost(post.get());
+//                if( likeService.findLikeByPostAndAndUser(post.get() , user.get()) != null) {
+//                    System.out.println("exist");
+//                    likeService.deleteLike( post.get().getId(), user.get().getId());
+//                }
+//                else
+//                    likeService.save(like);
+//               return "redirect:/post/"+ like.getPost().getId();
+//
+//            } else {
+//                return "/error";
+//            }
+//
+//        } else {
+//            return "/error";
+//        }
     }
 }
