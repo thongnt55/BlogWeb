7519a93ea35a0ec91833400d0396ba617ce09556
Nguyễn Tuấn Thông <109049076+thongnt55@users.noreply.github.com>
2022-10-10 11:22:00 +0700

    fix AuthenticationManager not found

--- src/main/java/com/blog/config/SpringSecurityConfig.java
+++ src/main/java/com/blog/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -26,7 +27,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     private final AccessDeniedHandler accessDeniedHandler;
 
     final DataSource dataSource;
-
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
     @Value("${spring.admin.username}")
     private String adminUsername;
 
--- src/main/java/com/blog/controller/LoginController.java
+++ src/main/java/com/blog/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+    @Autowired
+    AuthenticationManager authenticationManager;
     @GetMapping("/login")
     public String login(Authentication authentication) {
         System.out.println("authentication:" + authentication);
